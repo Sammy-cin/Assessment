834a6f78faedc008c5d71efd1dc9a668b4d6915a
Sammy-cin <samjoymaj@gmsil.com>
2021-11-20 00:01:33 +0530

    added successfully

---
+++ Foodbook.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+public class Foodbook {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver94.exe");
+		//access the url case
+		ChromeOptions option = new ChromeOptions ();
+	    option.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(option);
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.get("https://www.eazydiner.com/");
+		
+		
+		WebElement we=driver.findElement(By.id("home-search"));
+		we.click();
+    	we.sendKeys("indraprastha");
+    	Thread.sleep(2000);
+    	we.submit();
+     	driver.findElement(By.xpath("//button[@id='apxor_search']")).click();	
+    	
+    	}
+
+}
